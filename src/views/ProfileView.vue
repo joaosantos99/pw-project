b8f8756bf3852a5519ac483dec38f12d0f9d423f
João Santos <68588265+joaosantos99@users.noreply.github.com>
2025-11-19 22:43:21 +0000

    feat: enable logout in the profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,11 @@
 <script>
+  import { LogOutIcon } from 'lucide-vue-next'
+  import { mapActions } from 'pinia'
+
   import ContentHeader from '../components/ContentHeader.vue'
   import Button from '../components/primitives/Button.vue'
   import PageWrapper from '../components/PageWrapper.vue'
+  import { useAuthStore } from '../stores/auth'
 
   export default {
     name: 'ProfileView',
@@ -11,6 +15,18 @@
       Button,
       PageWrapper,
     },
+
+    data: () => ({
+      LogOutIcon,
+    }),
+
+    methods: {
+      ...mapActions(useAuthStore, ['logout']),
+      async handleLogout() {
+        await this.logout()
+        this.$router.push('/login')
+      },
+    },
   }
 </script>
 
@@ -19,6 +35,9 @@
     <ContentHeader
       title="Profile & Settings"
       paragraph="Manage your account and preferences"
+      buttonLabel="Logout"
+      :buttonIcon="LogOutIcon"
+      :onClick="handleLogout"
     />
     <div class="flex flex-col gap-6"></div>
   </PageWrapper>

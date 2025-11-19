3e3bda1dbfeeb15fcbec30ad34b451e9b3f0e1ea
João Santos <68588265+joaosantos99@users.noreply.github.com>
2025-11-19 22:20:24 +0000

    feat: auth logic
    fix

---
+++ src/constants/urls.js
@@ -0,0 +1,8 @@
+import { API_URL } from './env'
+
+export const apiUsersUrl = `${API_URL}/users`
+
+export const apiLoginUrl = apiUsersUrl
+export const apiSignupUrl = apiUsersUrl
+
+export const apiSessionsUrl = `${API_URL}/sessions`

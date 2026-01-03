d44e69b8f5d51443bdd8c19d71f8e6c5b1a697ff
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-01-03 20:39:37 +0000

    feat: initial setup for charts

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,20 @@
+<script>
+import { VisXYContainer, VisArea } from '@unovis/vue'
+
+const svgDefs = `
+    <linearGradient id="gradient" gradientTransform="rotate(90)">
+      <stop offset="20%" stop-color="#051937" />
+      <stop offset="40%" stop-color="#004d7a" />
+      <stop offset="60%" stop-color="#008793" />
+      <stop offset="80%" stop-color="#00bf72" />
+    </linearGradient>
+  `
+const x = (d) =>  d.x
+const y = (d) =>  d.y
+</script>
+
+<template>
+  <VisXYContainer :svgDefs="svgDefs" :data="data">
+    <VisArea :x="x" :y="y" color="url(#gradient)" />
+  </VisXYContainer>
+</template>

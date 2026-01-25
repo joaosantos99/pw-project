4a6b924c4d90975dc36ddf575ff64299f551e906
João Rego <116601075+joaorego01@users.noreply.github.com>
2026-01-25 18:32:46 +0000

    fix: miss placement of skip menu

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,9 @@ export default {
 </script>
 
 <template>
+	<a href="#main-content" class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-brand-primary focus:text-white focus:rounded">
+  	Skip menu
+  </a>
   <div v-if="shouldShowLayout" class="w-full flex">
     <Sidebar :is-open="isMenuOpen" @close="isMenuOpen = false"/>
     <div class="w-full lg:ml-[280px]">
